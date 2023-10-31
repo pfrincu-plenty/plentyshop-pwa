ce362f3fa43f237ce333a8c7e3ae6d898965f7a0
doproiescu-plenty <126688988+doproiescu-plenty@users.noreply.github.com>
2023-10-31 10:34:30 +0200

    feat: add support file and needed packages

--- apps/web/__tests__/support/e2e.ts
+++ apps/web/__tests__/support/e2e.ts
@@ -1,2 +1,12 @@
 import 'cypress-real-events';
 import './commands';
+
+import failOnConsoleError, { Config } from 'cypress-fail-on-console-error'
+
+const config: Config = {
+    consoleMessages: [/.*useUser\/login.*/, /.*paypal.*/],
+    consoleTypes: ['error'],
+    debug: false
+};
+
+failOnConsoleError(config);
