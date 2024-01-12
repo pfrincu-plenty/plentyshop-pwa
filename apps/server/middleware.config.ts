73a82dab3a9c14a1deb49e46dccc333f3bc051ed
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2024-01-12 11:02:33 +0100

    feat: testing env

--- apps/server/middleware.config.ts
+++ apps/server/middleware.config.ts
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
-import * as path from 'path';
+/* import * as path from 'path';
 
 dotenv.config({
   path: path.resolve(__dirname, '../web/.env'),
-});
+}); */
 
 const config = {
   integrations: {
