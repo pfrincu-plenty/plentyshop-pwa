3e46dd3408da632072995beeb26c14fbfd759278
Petrisor Frincu <petrisor.frincu@plentysystems.com>
2024-10-01 14:55:48 +0300

    Email confirmation

---
+++ apps/web/pages/email-confirmation.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full p-8 overflow-x-auto text-center">
+    {{ $t('emailConfirmation.newsletterOptInMessage') }}
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default',
+  pageType: 'static',
+});
+</script>
